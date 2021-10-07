2e72ca079957997be9239c2559c35015c2253d24
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-07 22:17:14 +0300

    update main.dart with variable locations

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/currentWeather.dart';
 
+import 'models/location.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  List<Location> locations = [
+    new Location(city: "minsk", country: "belarus", lat: "53,9", lon: "27,5667")
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CurrentWeatherPage(),
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CurrentWeatherPage(locations, context),
     );
   }
 }
