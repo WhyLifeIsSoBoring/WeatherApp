e94e83c3ac7431bca0b384b5e3ca8c32b80cca90
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-07 22:14:51 +0300

    add model location.dart

---
+++ lib/models/location.dart
@@ -0,0 +1,8 @@
+class Location {
+  String city;
+  String country;
+  String lat;
+  String lon;
+  
+  Location({required this.city, required this.country, required this.lat, required this.lon,});
+}
