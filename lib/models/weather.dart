b8ab546b658d8a7a2a3865ffeab59faafdb3fa43
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-07 11:05:55 +0300

    add factory to parse json into the model and generated the instance of Weather class

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -6,4 +6,14 @@ class Weather {
   final String description;
 
   Weather({required this.temperature, required this.feelsLike, required this.low, required this.high, required this.description});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      temperature: json['main']['temp'].toDouble(),
+      feelsLike: json['main']['feels_like'].toDouble(),
+      low: json['main']['temp_min'].toDouble(),
+      high: json['main']['temp_max'].toDouble(),
+      description: json['weather'][0]['description'],
+    );
+  }
 }
