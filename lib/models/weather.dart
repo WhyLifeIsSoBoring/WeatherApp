bcd5a56d1b687ea11480fa3d34a33e88940979b6
Maksim Stankevich <maksimka.stankevich.2001@gmail.com>
2021-10-07 10:52:29 +0300

    add class Weather

---
+++ lib/models/weather.dart
@@ -0,0 +1,9 @@
+class Weather {
+  final double temperature;
+  final double feelsLike;
+  final double low;
+  final double high;
+  final String description;
+
+  Weather({required this.temperature, required this.feelsLike, required this.low, required this.high, required this.description});
+}
